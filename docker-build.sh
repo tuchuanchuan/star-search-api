ff8563909e45c467c2d7a80428e674d2dc4d29f8
tcc <tuchuanchuan@kanjian.com>
2017-11-03 14:53:50 +0800

    docker build bash

---
+++ docker-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'compile && pack'
+docker build -t star-search-gradle -f Dockerfile-gradle .
+docker run -v --rm ${PWD}:/usr/src/app star-search-gradle
+
+echo 'docker build && push'
+datetime_str=`date +%Y%m%d%H%M`
+docker build -t registry.cn-hangzhou.aliyuncs.com/kanjian/star-search-api:$datetime_str .
+docker push registry.cn-hangzhou.aliyuncs.com/kanjian/star-search-api:$datetime_str
